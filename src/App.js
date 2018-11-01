03bf75752727a41c64b82e630e2620560c469918
Pia Lindkjølen <pialindkjolen@gmail.com>
2018-11-01 10:21:49 +0100

    Woho, fetching data and displaying it

--- src/App.js
+++ src/App.js
@@ -17,26 +17,46 @@ const config = client.config()
 console.log('dataset',config.dataset)
 client.config()
 
-const query = '*'
-const params = {}
+const query = '*[_type==$type]'
+const params = {type:'restaurant'}
 const options = {includeResult: true};
+//let restaurants;
+let resList = [];
+var restaurants = [];
 
 const subscription = client.listen(query, params)
   .subscribe(change => {
     console.log('change', change)
+    console.log('restaurants', restaurants);
   })
 client.listen(query, params, options)
+
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) =>
+  <li>{number}</li>
+);
+
 class App extends Component {
+  componentDidMount() {
+    const self = this;
+    client.fetch(query, params).then(res => {
+      //console.log(restaurants);
+      restaurants = res.map(restaurant => {
+        return <li key={restaurant.name}>{restaurant.name}</li>
+      });
+      
+      self.setState({data: restaurants});
+    });
+  }
   render() {
+    
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-        Introintro
-        </p>
+        { this.state && this.state.data && <ul>{restaurants}</ul>}
       </div>
     );
   }
